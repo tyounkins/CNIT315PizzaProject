412bd2462cd5c7222715ccea1cb6fc1ff9c590f6
taylor younkins <dottedtaylor@gmail.com>
2019-11-11 11:15:59 -0500

    write out api response to txt, added menu

--- main.c
+++ main.c
@@ -14,14 +14,23 @@ int main () {
     /* First set the URL that is about to receive our POST. This URL can
        just as well be a https:// URL if that is what should receive the
        data. */ 
-    curl_easy_setopt(curl, CURLOPT_URL, "https://order.dominos.com/power/store-locator?s=&c=47906&type=Delivery");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://order.dominos.com/power/store-locator?s=&c=47907&type=Delivery");
     /* Now specify the POST data */ 
     // curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "?s=&c=47906&type=Delivery"); 
  
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
+    printf("\n\n\ntest\n");
 
-    printf(res);
+    /* Perform the request, res will get the return code */ 
+
+    //writes out the response to target.txt
+    FILE *f = fopen("target.txt", "wb");
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
+    char * HELP = curl_easy_perform(curl);
+
+    // printf("help %s", HELP);
+    // res = curl_easy_perform(curl);
+
+    //implemenet a way to filter down curl_easy_perform    
 
     /* Check for errors */ 
     if(res != CURLE_OK)
---
+++ menu.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main  (){
+
+    printf("\nHi! Welcome to the Domino's Pizza Ordering Application!\n");
+    printf("What would you like to do?\n");
+
+    /**
+    
+    find closest {address info} can be full or partial address city and state, postal code etc...
+
+    find near {address info} can be full or partial address city and state, postal code etc...
+
+    menu for closest {address info} can be full or partital address city and state, postal code etc...
+
+    full menu for closest {address info} can be full or partial address city and state, postal code etc...
+
+    menu for {storeID} get store id as part of a find closest or find near request
+
+    full menu for {storeID} get store id as part of a find closest or find near request
+
+    order {comma deliminated list of item codes} 
+    example:
+    order PXC_14SCREEN, 2LSPRITE !!!!get item codes from menu
+    */
+
+    return 0;
+}

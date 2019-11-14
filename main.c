c5720e08d29d1f9b5bb7141ebcbf79c73ed8ff51
taylor younkins <dottedtaylor@gmail.com>
2019-11-14 15:12:59 -0500

    almost parses nearby store data

--- main.c
+++ main.c
@@ -1,45 +1,121 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <string.h>
 
-int main () {
+print(void *ptr, size_t size, size_t nmemb, void *stream){
+    printf("whats up");
+    // printf("my string:\n %s\n\n",ptr);
 
-    printf("hello\n");    
-    printf("lets do some test api calls :)\n");
-     CURL *curl;
+    int length = 229;
+
+    char str3[length];
+
+    // strncpy ( str3, ptr, 70 );
+    // str3[70] = '\0';   /* null characte
+    // r manually added */
+    // printf("\n\nthis is my string: %s", str3);
+
+    // char str[] ="This is a simple string";
+    char * pch;
+    pch = strstr (ptr,"\"StoreID");
+
+    strncpy( str3, pch, length);
+    str3[length] = "\0";
+
+    printf("\nstr3:%s\n\n", str3);
+    // printf("\nHelp\n");
+
+    // printf(")
+
+    int count = 0; //like a boolean
+
+    for(int i = 0; i < length; i++){
+      // printf("Hello");
+      if(str3[i] == '"'){ //|| str3[i] == '{' 
+
+        if(count == 4){
+          printf("\n");
+          count = 0;
+        } else {
+          count++;
+        }
+      
+        // printf("flag is : %d", flag);
+      } else if (str3[i] == '\\' && str3[i+1] == 'n'){
+        printf("\n");
+        i++;
+      } else {
+        printf("%c",str3[i]);
+      }
+
+    }
+  printf("\n");
+
+
+}
+
+int main()
+{
+
+  printf("hello\n");
+  printf("lets do some test api calls :)\n");
+  CURL *curl;
   CURLcode res;
- 
-  /* get a curl handle */ 
+
+  /* get a curl handle */
   curl = curl_easy_init();
-  if(curl) {
+  if (curl)
+  {
     /* First set the URL that is about to receive our POST. This URL can
        just as well be a https:// URL if that is what should receive the
-       data. */ 
+       data. */
     curl_easy_setopt(curl, CURLOPT_URL, "https://order.dominos.com/power/store-locator?s=&c=47907&type=Delivery");
-    /* Now specify the POST data */ 
-    // curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "?s=&c=47906&type=Delivery"); 
- 
-    printf("\n\n\ntest\n");
+    /* Now specify the POST data */
+    // curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "?s=&c=47906&type=Delivery");
 
-    /* Perform the request, res will get the return code */ 
+    // printf("\n\n\ntest\n");
+
+    /* Perform the request, res will get the return code */
 
     //writes out the response to target.txt
     FILE *f = fopen("target.txt", "wb");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
-    char * HELP = curl_easy_perform(curl);
+    // curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, print);
+    res = curl_easy_perform(curl);
+
+#define CHUNK 2048 /* read 1024 bytes at a time */
+    char buf[CHUNK];
+    // FILE *file;
+    size_t nread;
+
+    FILE *readptr = fopen("target.txt", "r");
+    // file = fopen("test.txt", "r");
+    if (readptr)
+    {
+      while ((nread = fread(buf, 1, sizeof buf, readptr)) > 0)
+
+        // printf("%s");
+      // fwrite(buf, 1, nread, stdout);
+      if (ferror(readptr))
+      {
+        /* deal with error */
+      }
+      fclose(readptr);
+    }
 
     // printf("help %s", HELP);
     // res = curl_easy_perform(curl);
 
-    //implemenet a way to filter down curl_easy_perform    
+    //implemenet a way to filter down curl_easy_perform
 
-    /* Check for errors */ 
-    if(res != CURLE_OK)
+    /* Check for errors */
+    if (res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
- 
-    /* always cleanup */ 
+
+    /* always cleanup */
     curl_easy_cleanup(curl);
   }
   curl_global_cleanup();
-    return 0;
+  return 0;
 }
--- menu.c
+++ menu.c
@@ -1,12 +1,126 @@
 #include <stdio.h>
-#include <curl/curl.h>
+/******************************************************
+Jairius Davenport 11-12-19
+Taylor Younkins 11-13-19
+*******************************************************/
+void placeOrder();
+void processOrder();
+void searchNear();
+void displayMenu();
+const int a = 10, b = 100, c = 30, d = 5, e = 50;
 
-int main  (){
+char userAddr[50];
+char userFName[25];
+char userLName[35];
+char userEmail[50];
+char userPhone[10];
+char userCardNum[16];
+char userCardX[5];
+char userCardSec[4];
+char userPost[10];
+char order[30];
+char locationAddr[30];
+char locationCity[25];
+char locationState[2];
+char locationZip[8];
+char menuItems[50];
 
+int main()
+{
+    int end = 0;
+
+    int choice;
     printf("\nHi! Welcome to the Domino's Pizza Ordering Application!\n");
-    printf("What would you like to do?\n");
+    do
+    {
+        printf("What would you like to do?\n1. Search nearest locations\n2. Display Menu\n3. Place Order");
+        printf("\nI'd like to choose option: ");
+        scanf("%d", &choice);
 
-    /**
+        switch (choice)
+        {
+        case 1:
+            searchNear();
+            break;
+
+        case 2:
+            displayMenu();
+            break;
+
+        case 3:
+            placeOrder();
+            processOrder();
+            break;
+        }
+
+        printf("\nWould you like to exit? (1=yes 0=no): ");
+        scanf("%d", &end);
+    } while (end != 1);
+} //end of main
+
+void processOrder()
+{
+    printf("To begin processing your order we'll need some basic information\n");
+    printf("Full delivery address: ");
+    scanf("%s", userAddr);
+    printf("First Name: ");
+    scanf("%s", userFName);
+    printf("Last Name: ");
+    scanf("%s", userLName);
+    printf("E-mail address: ");
+    scanf("%s", userEmail);
+    printf("Phone number: ");
+    scanf("%s", userPhone);
+    printf("\nNext will prompt you for payment information \n");
+    printf("\nCredit card number: ");
+    scanf("%s", userCardNum);
+    printf("Credit card expiration date (mm/yy): ");
+    scanf("%s", userCardX);
+    printf("Credit card security code or CVV (3 to 4 digit code): ");
+    scanf("%s", userCardSec);
+    printf("Postal code: ");
+    scanf("%s", userPost);
+
+}
+
+void placeOrder()
+{
+    printf("Begin your order by selecting items\n");
+
+    //API code goes here??
+}
+
+void searchNear()
+{
+    printf("To find a location near you, please input your location\n");
+    printf("\nStreet address: ");
+    scanf("%s", locationAddr);
+    printf("\nCity: ");
+    scanf("%s", locationCity);
+    printf("\nState: ");
+    scanf("%s", locationState);
+    printf("\nZip: ");
+    scanf("%s", locationZip);
+
+    printf("Your address:\nStreet: %s\nCity: %s\nState: %s\nZip: %s", locationAddr, locationCity,locationState,locationZip);
+
+    //API code goes here??
+    printf("The store closest to you is: ");
+}
+
+void displayMenu()
+{
+    printf("Here is the menu selection from your nearest store\n");
+    printf("Store_ID: %s", locationZip); //the value in location is completely a placeholder until we have the actual store ID
+
+    //API code goes here??
+}
+
+//prompt using numbers
+//save every input into a string
+//error handling is for Alonzo
+
+/**
     
     find closest {address info} can be full or partial address city and state, postal code etc...
 
@@ -22,8 +136,15 @@ int main  (){
 
     order {comma deliminated list of item codes} 
     example:
-    order PXC_14SCREEN, 2LSPRITE !!!!get item codes from menu
-    */
+    order PXC_14SCREEN, 2LSPRITE
+    what is the full address for delivery? 205 N Russel Street, West lafayette, IN, 47907
+    First Name? Taylor
+    Last Name? Younkins
+    E-Mail? tayloryounkins@gmail.com
+    Phone number? 4104561552
+    Credit Card Number? 34824347983742937
+    Credit Card Expiration? 3242
+    Credit Card Security Code or CVV (3 or 4 digit code on card)? 23423
+    Postal Code? 47906-4241
 
-    return 0;
-}
+    */
